bce1176db94787a4589f1f0619883c6dd732873b
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-07-17 15:46:52 +0300

    Добавьте файлы проекта.

---
+++ Twice linear/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class DoubleLinear
+{
+    public static void Main()
+    {
+        // Test
+        var t = DblLinear(10);
+        // ...should return 22
+    }
+
+    public static int DblLinear(int n)
+    {
+        int x = 1;
+        SortedSet<int> sequence = new() { x };
+
+        for (int i = 0; i <= n; i++)
+        {
+            x = sequence.ElementAt(0);
+            sequence.Remove(x);
+            sequence.Add(2 * x + 1);
+            sequence.Add(3 * x + 1);
+        }
+
+        return x;
+    }
+}
